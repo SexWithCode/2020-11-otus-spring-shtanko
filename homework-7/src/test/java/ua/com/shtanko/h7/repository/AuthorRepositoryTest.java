9298f56c3d3ea980333eef68df4a857ad92d681b
SexWithCode <sexwithcode@gmail.com>
2021-02-24 06:03:20 +0200

    [Homework 7] Correct tests and remove JUnit-4 from dependencies.

--- homework-7/src/test/java/ua/com/shtanko/h7/repository/AuthorRepositoryTest.java
+++ homework-7/src/test/java/ua/com/shtanko/h7/repository/AuthorRepositoryTest.java
@@ -1,22 +1,19 @@
 package ua.com.shtanko.h7.repository;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import ua.com.shtanko.h7.domain.entity.Author;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @DisplayName("Author repository Spring Data Jpa implementation should :")
-public class AuthorRepositoryTest {
+class AuthorRepositoryTest {
     public static final Long EXPECTED_AUTHOR_ID = 100L;
     public static final String EXPECTED_AUTHOR_NAME = "John R. R. Tolkien";
 
@@ -25,7 +22,7 @@ public class AuthorRepositoryTest {
 
     @DisplayName("find author by its name")
     @Test
-    public void shouldFindAuthorByName() {
+    void shouldFindAuthorByName() {
         Author actualAuthor = authorRepository.findByName("John R. R. Tolkien")
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] Unknown author."));
 
--- homework-7/src/test/java/ua/com/shtanko/h7/repository/GenreRepositoryTest.java
+++ homework-7/src/test/java/ua/com/shtanko/h7/repository/GenreRepositoryTest.java
@@ -1,22 +1,19 @@
 package ua.com.shtanko.h7.repository;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import ua.com.shtanko.h7.domain.entity.Genre;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @DisplayName("Genre repository Spring Data Jpa implementation should :")
-public class GenreRepositoryTest {
+class GenreRepositoryTest {
     public static final Long EXPECTED_GENRE_ID = 100L;
     public static final String EXPECTED_GENRE_NAME = "Fantasy";
 
@@ -25,7 +22,7 @@ public class GenreRepositoryTest {
 
     @DisplayName("find genre by its name")
     @Test
-    public void shouldFindGenreByName() {
+    void shouldFindGenreByName() {
         Genre actualGenre = genreRepository.findByName("Fantasy")
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] Unknown genre."));
 
