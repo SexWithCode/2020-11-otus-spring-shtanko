b0fe697a076d295e4a1b46ae4bc3d83ab18b1328
SexWithCode <sexwithcode@gmail.com>
2021-02-26 14:33:33 +0200

    [Homework 7] Remove Query annotation. Done. Ready for review (2).

--- homework-7/src/main/java/ua/com/shtanko/h7/repository/GenreRepository.java
+++ homework-7/src/main/java/ua/com/shtanko/h7/repository/GenreRepository.java
@@ -1,12 +1,10 @@
 package ua.com.shtanko.h7.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ua.com.shtanko.h7.domain.entity.Genre;
 
 import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-    @Query("select a from Genre a where a.name = :name")
     Optional<Genre> findByName(String name);
 }
